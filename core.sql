50026fbd84dcc4b7a65c15da79179a2624f21ce2
ralzakark <alessandro.caputo@gmail.com>
2013-03-23 16:07:41 +0100

    modified:   core.sql

--- core.sql
+++ core.sql
@@ -1178,6 +1178,12 @@ CREATE TABLE "committee" (
 		"description" text NOT NULL DEFAULT ''::text,
 		"text_search_data" tsvector,
 		CONSTRAINT committe_pkey PRIMARY KEY (id));
+CREATE INDEX "committee_text_search_data_idx" ON "committee" USING gin ("text_search_data");
+CREATE TRIGGER "update_text_search_data"
+  BEFORE INSERT OR UPDATE ON "commitee"
+  FOR EACH ROW EXECUTE PROCEDURE
+  tsvector_update_trigger('text_search_data', 'pg_catalog.simple',
+    "name", "description");
 COMMENT ON TABLE committee
   IS 'Committees formed to do research and make recommendations on a potential or planned project or change. ';
 COMMENT ON COLUMN committee.name IS 'Committee description';
